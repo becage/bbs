4b300a6a8c03c1e114de13ca1cead39acb4f19da
Cage <becage@qq.com>
2019-03-19 17:37:05 +0800

    curl

--- app/Handlers/WechatTextMsgHandler.php
+++ app/Handlers/WechatTextMsgHandler.php
@@ -34,7 +34,7 @@ class WechatTextMsgHandler implements EventHandlerInterface
             $response = app(TuringRobotHandler::class)->chat($payload['Content']);
             $str = $response['first'];
             if (preg_match('/笑话+/', $payload['Content'])) {
-                Log::info('1.');
+                Log::info($response);
                 $img = app(ImageUploadHandler::class)->downfile($response['second'], 'storage/turings/joke.jpg');
                 Log::info('2.');
                 $img = Inimage::make($img);
